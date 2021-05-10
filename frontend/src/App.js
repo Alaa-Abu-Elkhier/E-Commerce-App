b94ea036cce4c83b1a7bc371379737dbb2af6390
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2021-05-10 23:27:50 +0200

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,35 @@
 
 import './App.css';
-import Home from "./Home";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import NotFound from './NotFound';
+import ProductScreen from './screens/ProductScreen';
+import HomeScreen from './screens/HomeScreen';
 
 function App() {
 
   return (
-    <Router>
-    <div className="App">
-      {/* <Navbar /> */}
-      <div className="content">
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          {/* <Route path="/create">
-            <Create></Create>
-          </Route>
-          <Route path="/blogs/:id">
-            <BlogDetails></BlogDetails>            
-          </Route> */}
-          <Route path="*">
-            <NotFound ></NotFound>           
-          </Route>
-        </Switch>
-      </div>
-    </div>
-  </Router>
+    <Router> 
+    <div className="grid-container">
+     <header className="row">
+       <div>
+         <a className="brand" href="/">amazona</a>
+       </div>
+       <div>
+         <a href="/cart">Cart</a>
+         <a href="/signin">Sign In</a>
+       </div>
+     </header>
+     <main>
+      <Route path="/" component={HomeScreen} exact></Route>
+      <Route path="/product/:id" component={ProductScreen}></Route>
+      {/* <Route path="*"  component={NotFound}></Route>    */}
+               
+      
+     </main>
+     <footer className="row center">All right reserved</footer>
+   </div>
+   </Router>  //<BrowserRputer> but we import it as Router
+  
   );
 }
 
