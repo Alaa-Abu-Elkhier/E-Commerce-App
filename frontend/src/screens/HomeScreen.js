4c470267c7533adaf411dbe810bdf7cc7c72c74a
Alaa-Abu-Elkhier <37739113+Alaa-Abu-Elkhier@users.noreply.github.com>
2021-05-11 22:11:38 +0200

    Update HomeScreen.js
    send request to server for fetching data and handling  error message and loading data

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,18 +1,53 @@
+import { useEffect, useState } from "react";
+import axios from'axios';
 import Products from "../components/Products";
-import data from "../data";
+import  LoadingBox  from "../components/LoadingBox";
+import  MessageBox  from "../components/MessageBox";
+
+
+
 
 const HomeScreen = () => {
+    const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false)
+    useEffect(()=>{
+        const fetchData= async()=>{
+            
+            
+            try{
+                setLoading(true);
+                const{data}=await axios.get('http://localhost:4000/api/products');
+                setLoading(false);
+                setProducts(data);
+
+            }catch(err){
+                setError(err.message)
+                setLoading(false);
+
+            }
+            
+        };
+        fetchData();
+    },[])
+    
     return ( 
         <div>
+        {loading? <LoadingBox></LoadingBox>
+        :
+        error? <MessageBox variant="danger">{error}</MessageBox>
+        :
         <div className="row center">
         { 
-            data.products.map((product)=>
+            products.map((product)=>
             <Products key={product._id} product={product}></Products>
             )
         }
         
         
       </div>
+        }
+        
       </div>
      );
 }
